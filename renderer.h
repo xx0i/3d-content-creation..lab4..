26a512d0b18d38672066d405bdeb25a5613be4bb
unknown <natalie9705@gmail.com>
2024-10-08 21:23:16 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -72,6 +72,7 @@ public:
 		win = _win;
 		vlk = _vlk;
 		UpdateWindowDimensions();
+		GetHandlesFromSurface();
 
 		// TODO: Part 2a
 		interfaceProxy.Create();
@@ -142,7 +143,6 @@ private:
 
 	void InitializeGraphics()
 	{
-		GetHandlesFromSurface();
 		InitializeVertexBuffer();
 		// TODO: Part 1g
 		InitializeIndexBuffer();
